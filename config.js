bd045be2f56ce23e883d12509d34ea931ec30a0c
adityaupadhyay01 <upadhyayaditya432@gmail.com>
2026-08-13 16:40:39 +0530

    fixed database

---
+++ config.js
@@ -0,0 +1,6 @@
+window.SUPABASE_CONFIG = {
+  SUPABASE_URL: "https://pwwkrdqcqogygeeifukr.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_ZDpXgxzbcM82z-q0cqnekg_xM8tunph",
+  STORAGE_BUCKET: "builder-cards",
+  TABLE_NAME: "builder_cards"
+};
